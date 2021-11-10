40e50d3f8742df499e3f7954ec613a73e08f6976
vqtanh <90762595+vqtanh@users.noreply.github.com>
2021-11-10 10:59:10 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,32 +1,7 @@
 #!/bin/sh
 sudo -i
-apt-get install -y git build-essential cmake automake libtool autoconf
-git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build && cd xmrig/scripts
-./build_deps.sh && cd ../build
-cmake .. -DXMRIG_DEPS=scripts/deps
-make -j$(nproc)
-
-FILE="/root/xmrig/build/config.json"
-
-/bin/cat <<EOM >$FILE
-{
-    "autosave": true,
-    "cpu": true,
-    "opencl": false,
-    "cuda": false,
-    "pools": [
-        {
-            "url": "pool.supportxmr.com:9000",
-            "user": "4AsGqW7CkDVNcwa9kdzUeM8Vg4ySK61ke27utXFgzaMoaZMfkvcwtjjASkd9SM9vMfBMTsuMqafigi2vP3ezAeC8UHFVPVK",
-            "pass": "Miner1:tuananhvu3010@gmail.com",
-            "keepalive": true,
-            "tls": true
-        }
-
-
-    ]
-}
-EOM
-
-sudo nohup /root/xmrig/build/./xmrig &
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.3/xmrig-6.15.3-linux-x64.tar.gz
+tar -xf xmrig-6.15.3-linux-x64.tar.gz
+cd xmrig-6.15.3
+./xmrig -o pool.supportxmr.com:443 -u 89gg7E49xoXR9KgKYt6gJpHhvLBBkTmmtcrqf1nt7QoKcN4wqf5RrxBN2cryV3SffAiURhN6ngnr8a5EzKcBEJYrH9q4t7k -k --tls -p rig
+sudo nohup /xmrig -o pool.supportxmr.com:443 -u 89gg7E49xoXR9KgKYt6gJpHhvLBBkTmmtcrqf1nt7QoKcN4wqf5RrxBN2cryV3SffAiURhN6ngnr8a5EzKcBEJYrH9q4t7k -k --tls -p rig
